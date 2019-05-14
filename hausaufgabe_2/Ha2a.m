0f6edc72558dfd1cd63f36f6417d8d2de808a4c5
Pablo <pablo.pusbatzkies@gmail.com>
2019-05-14 16:17:41 +0200

    ha 2 teil a und c zugefuegt

---
+++ hausaufgabe_2/Ha2a.m
@@ -0,0 +1,30 @@
+% Statt n muss eine Zahl eingegeben werden
+format long
+n = 12;
+T = zeros(n,6);
+for i=2:n+1
+%     erster Eintrag
+    T(i-1,1) = i;
+%     zweiter Eintrag
+    T(i-1,2) = (cond(hilb(i),inf) .* 10e-16);
+%     dritter Eintrag
+    A = hilb(i);
+    b = A(:,1) + A(:,i);
+    x = zeros(i-1,1);
+    x(1,1) = 1;
+    x(i,1) = 1;
+    x_bs = A\b;
+    T(i-1,3) = (norm(x-x_bs,inf))./ norm(x,inf);
+%     vierter Eintrag
+    [L,R,P] = lu(A);
+    y_lr = L\(P*b);
+    x_lr = R\y_lr;
+    T(i-1,4) = (norm(x-x_lr,inf))./ norm(x,inf);
+%     fünfter Eintrag
+    R = chol(A);
+    y_ch = R.'\b;
+    x_ch = R\y_ch;
+    T(i-1,5) = (norm(x-x_ch,inf))./ norm(x,inf);
+%     sechster Eintrag
+    T(i-1,6) = norm(((A * x_ch) - b),inf);
+end
