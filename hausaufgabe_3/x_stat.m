d5da2928cc6cddaab074076bfb5e287e8b4f3877
Tavary <50634501+Tavary@users.noreply.github.com>
2019-05-19 18:52:22 +0200

    x_stat zugefuegt, FUNKTIONIERT NOCH NICHT!

---
+++ hausaufgabe_3/x_stat.m
@@ -0,0 +1,19 @@
+function [x]=x_stat(a1,a2,s1,s2,l1,l2,m,x0)
+    
+    g=9.81;
+    x=x0;
+    for i=1:1337
+        f1= -s1.*(norm(x-a1)-l1)*((x-a1)/(norm(x-a1)));
+        f2= -s2.*(norm(x-a2)-l2)*((x-a2)/(norm(x-a2)));
+        G=[0;-g.*m];
+        f1strich=s1.*((((l1./norm(x-a1))-1)*eye(2))-l1.*(((x-a1)*(x-a1)') /(norm(x-a1))^3));
+        f2strich=s2.*((((l2./norm(x-a2))-1)*eye(2))-l2.*(((x-a2)*(x-a2)') /(norm(x-a2))^3));
+
+        fges=f1+f2+G;
+        fstrich=f1strich + f2strich;
+
+        x=x-(fstrich \ fges);
+    end
+    
+end
+    
---
+++ hausaufgabe_3/x_stat_tester.m
@@ -0,0 +1,18 @@
+a1=[0;0];
+a2=[1;1];
+s1=10;
+s2=10;
+l1=2;
+l2=2;
+m=1;
+x0=[0 ;-4];
+
+x=x_stat(a1,a2,s1,s2,l1,l2,m,x0);
+disp(x);
+
+plot(a1(1),a1(2),'x','color','k')
+hold on
+plot(a2(1),a2(2),'x','color','k')
+plot(x0(1),x0(2),'o','color','b')
+plot(x(1),x(2),'o','color','g')
+hold off
