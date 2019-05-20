1c5f5c756414f5e64c02e85b6c47ea3fdfe9e5a5
Tavary <50634501+Tavary@users.noreply.github.com>
2019-05-20 20:19:02 +0200

    Julias Code geaddet und neue Testvariablen bei unserem Code gespeichert
    Julias Code geaddet und neue Testvariablen bei unserem Code gespeichert

--- hausaufgabe_3/x_stat_tester.m
+++ hausaufgabe_3/x_stat_tester.m
@@ -1,5 +1,5 @@
-a1=[0;0];
-a2=[1;1];
+a1=[1;5];
+a2=[-2;2];
 s1=10;
 s2=10;
 l1=2;
@@ -10,9 +10,9 @@ x0=[0 ;-4];
 x=x_stat(a1,a2,s1,s2,l1,l2,m,x0);
 disp(x);
 
-plot(a1(1),a1(2),'x','color','k')
-hold on
-plot(a2(1),a2(2),'x','color','k')
-plot(x0(1),x0(2),'o','color','b')
-plot(x(1),x(2),'o','color','g')
-hold off
+%plot(a1(1),a1(2),'x','color','k')
+%hold on
+%plot(a2(1),a2(2),'x','color','k')
+%plot(x0(1),x0(2),'o','color','b')
+%plot(x(1),x(2),'o','color','g')
+%hold off
