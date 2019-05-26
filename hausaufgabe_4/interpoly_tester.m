73b29f97c161b71a1d5d8a348df519cb5b8e2240
Tavary <50634501+Tavary@users.noreply.github.com>
2019-05-26 16:12:24 +0200

    ha 4 auf 1 interpolation hotfix

--- hausaufgabe_4/interpoly_tester.m
+++ hausaufgabe_4/interpoly_tester.m
@@ -3,9 +3,9 @@ clear
 
 a=-6;
 b=6;
-n=18;
-%d=@(x) cos(x);
-d=@(x) 1./(1+(x.^2));
+n=5;
+d=@(x) cos(x);
+%d=@(x) 1./(1+(x.^2));
 
 %Stuetzstellen aequidistant
 x_1=linspace(a,b,n)';
@@ -26,21 +26,9 @@ precision = 100;
 x_3=linspace(a,b,precision)';
 y_3=d(x_3);
 
-%aequidistante interpoly funktion
-y_1(precision,1)=0;
-for i=1:precision
-    for ii=1:n
-        y_1(i,1)=y_1(i,1) + (c_1(ii,1).*x_3(i,1).^(n+1-ii));
-    end
-end
+y_1 = polyval(c_1,x_3);
+y_2 = polyval(c_2,x_3);
 
-%tschebyscheff interpoly funktion
-y_2(precision,1)=0;
-for i=1:precision
-    for ii=1:n
-        y_2(i,1)=y_2(i,1) + (c_2(ii,1).*x_3(i,1).^(n+1-ii));
-    end
-end
 
 subplot(2,1,1)
 plot(x_3,y_3,'b')
@@ -48,8 +36,7 @@ hold on
 plot(x_3,y_1,'k')
 plot(x_1,f_1,'.','color','r')
 hold off
-axis([a b -5 5])
-axis equal
+axis([-6 6 -0.5 1.5])
 title('Interpolation mit aequidistanten Stuetzstellen')
 
 subplot(2,1,2)
@@ -58,7 +45,6 @@ hold on
 plot(x_3,y_2,'k')
 plot(x_2,f_2,'.','color','r')
 hold off
-axis([a b -5 5])
-axis equal
+axis([-6 6 -0.5 1.5])
 title('Interpolation mit Stuetzstellen nach Tschebyscheff')
 
