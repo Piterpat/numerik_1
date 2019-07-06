ce462f99e7a8ece6524433e1aa1e8016fc0b6d39
Tavary <50634501+Tavary@users.noreply.github.com>
2019-07-06 14:55:47 +0200

    keine ahnung, was hier der change ist

--- projekt/ha_3/ha_3_1_g.m
+++ projekt/ha_3/ha_3_1_g.m
@@ -4,12 +4,12 @@ function [z]=ha_3_1_g(x,y)
     z=zeros(n,m);
     for i=1:n
         for ii=1:m
-%             if sqrt(x(i).*y(ii)) <= 0.5
-%                 z(i,ii)=1;
-%             else
-%                 z(i,ii)=sqrt(x(i).*y(ii));
-%             end
-            z(i,ii)=(sin(5.*x(i)).*sin(5.*y(ii)))+5;
+            if sqrt(x(i).*y(ii)) <= 0.5
+                z(i,ii)=1;
+            else
+                z(i,ii)=0.5;
+            end
+%             z(i,ii)=(sin(5.*x(i)).*sin(5.*y(ii)))+5;
         end
     end
 end
