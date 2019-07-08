f4de445ed6678385c970e517b1668df53d5de735
Pablo <pablo.pusbatzkies@gmail.com>
2019-07-08 17:14:00 +0200

    kartenimplementierung in erhaltungschema

--- projekt/ha_3/ha_3_1_bodenterm.m
+++ projekt/ha_3/ha_3_1_bodenterm.m
@@ -1,5 +1,5 @@
-dBy=zeros(N-2,M-2);
-dBx=zeros(N-2,M-2);
+dBy=zeros(N,M);
+dBx=zeros(N,M);
 % 
-% dBx=(B(3:end,2:end-1)-B(1:end-2,2:end-1))./(2.*dx);
-% dBy=(B(2:end-1,3:end)-B(2:end-1,1:end-2))./(2.*dy);
+% dBx(2:end-1,2:end-1)=(B(3:end,2:end-1)-B(1:end-2,2:end-1))./(2.*dx);
+% dBy(2:end-1,2:end-1)=(B(2:end-1,3:end)-B(2:end-1,1:end-2))./(2.*dy);
